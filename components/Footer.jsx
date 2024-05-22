ffea08cbe7e60363e7621115eded5f549f2356bf
Allan Mejia <almesiete@gmail.com>
2024-05-21 23:11:00 -0600

    Styles modifications.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,9 +7,7 @@ import Button from './Button';
 const FooterLinks = ({ heading, items }) => (
   <div className="flex-1 justify-start items-start">
     <h3 className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl mb-10">{heading}</h3>
-    {items.map((item) => (
-      <p key={item} className="font-poppins dark:text-white text-nft-black-1 font-normal text-base cursor-pointer dark:hover:text-nft-gray-1 hover:text-nft-black-1 my-3">{item}</p>
-    ))}
+    {items.map((item, index) => <p key={item + index} className="font-poppins dark:text-white text-nft-black-1 font-normal text-base cursor-pointer dark:hover:text-nft-gray-1 hover:text-nft-gray-2 my-3">{item}</p>)}
   </div>
 );
 
@@ -18,7 +16,7 @@ const Footer = () => {
 
   return (
     <footer className="flexCenter flex-col border-t dark:border-nft-black-1 border-nft-gray-1 sm:py-8 py-16">
-      <div className="w-full minmd:w-4/5 flex flex-row md:flex-column sm:px-4 px-16">
+      <div className="w-full minmd:w-4/5 flex flex-row md:flex-col sm:px-4 px-16">
         <div className="flexStart flex-1 flex-col">
           <div className="flexCenter cursor-pointer">
             <Image
@@ -28,11 +26,11 @@ const Footer = () => {
               alt="logo"
               className="object-contain"
             />
-            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">FusionNFTs</p>
+            <p className="dark:text-white text-nft-dark font-semibold text-lg ml-1">FusionNFTs</p>
           </div>
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base mt-6">Get the latest updates</p>
-          <div className="flexBetween md:w-full minlg:w-577 w-357 mt-6 dark:bg-nft-black-2 bd-white border dark:border-nft-black-2 border-nft-gray-2 rounded-md">
-            <input type="email" placeholder="Your Email" className="h-full flex-1 w-full dark:bg-nft-black-2 bg-white px-4 rounded-md dark:text-white text-nft-black-1 font-normal text-xs minlg:text-lg outline-none" />
+          <div className="flexBetween md:w-full minlg:w-557 w-357 mt-6 dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 rounded-md">
+            <input type="email" placeholder="Your Email" className="h-full flex-1 w-full dark:bg-nft-black-2 bg-white px-4 rounded-md font-poppins dark:text-white text-nft-black-1 font-normal text-xs minlg:text-lg outline-none" />
             <div className="flex-initial">
               <Button btnName="Email me" classStyles="rounded-md" />
             </div>
@@ -41,18 +39,19 @@ const Footer = () => {
 
         <div className="flex-1 flexBetweenStart flex-wrap ml-10 md:ml-0 md:mt-8">
           <FooterLinks heading="FusionNFTs" items={['Explore', 'How it Works', 'Contact Us']} />
-          <FooterLinks heading="Support" items={['Help Center', 'Terms of Service', 'Legal', 'Privacy Policy']} />
+          <FooterLinks heading="Support" items={['Help Center', 'Terms of service', 'Legal', 'Privacy policy']} />
         </div>
       </div>
 
       <div className="flexCenter w-full mt-5 border-t dark:border-nft-black-1 border-nft-gray-1 sm:px-4 px-16">
         <div className="flexBetween flex-row w-full minmd:w-4/5 sm:flex-col mt-7">
-          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base">FusionNFTs, Inc. All Rights Reserved.</p>
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base">FusionNFTs, Inc. All Rights Reserved</p>
           <div className="flex flex-row sm:mt-4">
-            {[images.instagram, images.twitter, images.telegram, images.discord].map((image) => (
-              <div className="mx-2 cursor-pointer" key={image}>
+            {[images.instagram, images.twitter, images.telegram, images.discord].map((image, index) => (
+              <div className="mx-2 cursor-pointer" key={`image ${index}`}>
                 <Image
                   src={image}
+                  key={index}
                   className={`object-contain ${theme === 'light' && 'filter invert'}`}
                   width={24}
                   height={24}
